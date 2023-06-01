d5c8f86545545df01d111fb263c2464fea78d597
Colorfuls <bike.chanokchon@gmail.com>
2023-06-01 13:38:32 +0700

    add lab 01

--- main.cpp
+++ main.cpp
@@ -3,17 +3,6 @@
 int main()
 {
     std::cout << "Hello World" << std::endl;
+    
     return 0;
-}
-
-float lab1(float p) {
-    float k = 1;
-    float t = 1;
-
-    while (!((1 - t) >= p))
-    {
-        k = k + 1;        
-    }
-
-    return k;
 }
