a5e47c96c1ffa5fb185648182d82c2fd420f3f30
Colorfuls <bike.chanokchon@gmail.com>
2023-06-01 09:34:56 +0700

    add task BirthdayParadox

--- main.cpp
+++ main.cpp
@@ -5,3 +5,15 @@ int main()
     std::cout << "Hello World" << std::endl;
     return 0;
 }
+
+float lab1(float p) {
+    float k = 1;
+    float t = 1;
+
+    while (!((1 - t) >= p))
+    {
+        k = k + 1;        
+    }
+
+    return k;
+}
