3a50b01fc51b3a7c608400d611720d44faae3c73
Colorfuls <bike.chanokchon@gmail.com>
2023-06-02 15:54:27 +0700

    das

---
+++ 2_variable_and_datatypes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    int age = 21;
+    int year = 2023;
+    int days = 7;
+
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    char grade = 'A';
+    char initial = 'B';
+
+    bool isStudent = false;
+    bool power = true;
+    bool forSale = true;
+
+    std::string name = "chanokchon wongjampa";
+    std::cout << name;
+
+    return 0;
+}
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World" << std::endl;
-    return 0;
-}
-
-// https://youtu.be/-TkoO8Z07hI?t=719
